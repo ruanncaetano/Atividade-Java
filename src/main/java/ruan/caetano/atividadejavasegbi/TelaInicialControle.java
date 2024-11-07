73470337461c88041808005427b3f03658017091
ruanncaetano <ruan.c4825@gmai.com>
2024-11-07 19:07:10 -0300

    Exercicio de JavaFx feito em sala

---
+++ src/main/java/ruan/caetano/atividadejavasegbi/TelaInicialControle.java
@@ -0,0 +1,14 @@
+package ruan.caetano.atividadejavasegbi;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class TelaInicialControle {
+
+    @FXML
+    private TextField tfRegistro;
+
+    @FXML
+    private TextField tfSenha;
+
+}
